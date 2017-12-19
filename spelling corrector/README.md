f9b3caf6b4f37be3ebf6e85605e675084d060930
ahouzheng <34416893+ahouzheng@users.noreply.github.com>
2017-12-19 16:55:07 +0800

    how to use

--- spelling corrector/README.md
+++ spelling corrector/README.md
@@ -1,2 +1,8 @@
 this is a spelling corrector copy from "http://norvig.com/spell-correct.html"
 you can search "http://norvig.com/spell-correct.html" for details about this project
+
+example:
+
+out = correction("spele")
+print(out)
+
--- spelling corrector/spell.py
+++ spelling corrector/spell.py
@@ -1,38 +1,19 @@
-
-
-#  this is a spelling corrector copy from "http://norvig.com/spell-correct.html"
-#  you can search "http://norvig.com/spell-correct.html" for details about this project
-
-
 import re
 from collections import Counter
-
-
 def words(text): return re.findall(r'\w+', text.lower())
-
 WORDS = Counter(words(open('big.txt').read()))
-
-
 def P(word, N=sum(WORDS.values())):
   "Probability of `word`."
   return WORDS[word] / N
-
-
 def correction(word):
   "Most probable spelling correction for word."
   return max(candidates(word), key=P)
-
-
 def candidates(word):
   "Generate possible spelling corrections for word."
   return (known([word]) or known(edits1(word)) or known(edits2(word)) or [word])
-
-
 def known(words):
   "The subset of `words` that appear in the dictionary of WORDS."
   return set(w for w in words if w in WORDS)
-
-
 def edits1(word):
   "All edits that are one edit away from `word`."
   letters = 'abcdefghijklmnopqrstuvwxyz'
@@ -42,11 +23,7 @@ def edits1(word):
   replaces = [L + c + R[1:] for L, R in splits if R for c in letters]
   inserts = [L + c + R for L, R in splits for c in letters]
   return set(deletes + transposes + replaces + inserts)
-
-
 def edits2(word):
   "All edits that are two edits away from `word`."
   return (e2 for e1 in edits1(word) for e2 in edits1(e1))
-
-out = correction("spele")
-print(out)
+  
